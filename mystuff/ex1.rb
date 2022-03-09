3d2d381fcd817a3b6ad5fa628b0cd073a1348420
Maggie Fournier <mlfournier4@gmail.com>
2022-03-09 17:25:11 -0500

    Add files via upload
    Exercises Completed using Book Titled "Learn Ruby the Hard Way"

---
+++ mystuff/ex1.rb
@@ -0,0 +1,9 @@
+
+#this is my first ruby lesson
+puts "Hello World"
+puts "Hello Again"
+puts "I like typing this"
+puts "This is fun"
+puts "Yay! Printing"
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this'
