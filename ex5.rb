0e50370754dfd91e08658aa59ede59a7a94ec7cc
Maggie Fournier <mlfournier4@gmail.com>
2022-03-10 08:20:55 -0500

    Add files via upload
    Exercise 5

---
+++ ex5.rb
@@ -0,0 +1,45 @@
+
+#Maggie Fournier's personal information was not used for this tutorial
+
+my_name = 'Zed A. Shaw'
+my_age = 35
+my_height = 74 #inches
+my_weight = 180 #pounds
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts = "Lets talk about #{my_name}."
+puts = "He's #{my_height} inches tall."
+puts = "He's #{my_weight} pounds heavy."
+puts = "Actually, that's not too heavy."
+puts = "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts = "His teeth are usually #{my_teeth} depending on the coffee"
+
+# this line is tricky, try to get it exactly right
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight}, I get #{my_age + my_height + my_weight}."
+
+#Additional Study Drills Below
+
+
+#Maggie Fournier's personal information was not used for this tutorial
+#remove my_ from each variable
+name = 'Zed A. Shaw'
+age = 35
+height = 74 #inches
+weight = 180 #pounds
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts = "Lets talk about #{name}."
+puts = "He's #{height} inches tall."
+puts = "He's #{weight} pounds heavy."
+puts = "Actually, that's not too heavy."
+puts = "He's got #{eyes} eyes and #{hair} hair."
+puts = "His teeth are usually #{teeth} depending on the coffee"
+
+# this line is tricky, try to get it exactly right
+
+puts "If I add #{age}, #{height}, and #{weight}, I get #{age + height + weight}."
