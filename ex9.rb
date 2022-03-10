3a1794ab9bb9e9964af29dd483821a6e5589a793
Maggie Fournier <mlfournier4@gmail.com>
2022-03-10 09:34:14 -0500

    Add files via upload
    Exercise 9 - Print Statements Additional Practice Continued

---
+++ ex9.rb
@@ -0,0 +1,13 @@
+# More Printing Practice
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec\n"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+  There's something going on here.
+  with this weird quote
+  We'll be abel to type as much as we like.
+  Even 4 lines if we want, or 5, or 6.
+}
