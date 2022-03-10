782f4736e8190429a5469b7d22ba0d02a29e9147
Maggie Fournier <mlfournier4@gmail.com>
2022-03-10 08:38:10 -0500

    Add files via upload
    Exercise 6 Strings & Printing

---
+++ ex6.rb
@@ -0,0 +1,24 @@
+#Exercise 6 - Strings & Text
+types_of_people = 10
+binary = "binary"
+do_not = "don't"
+x = "There are #{types_of_people} types of people."
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said #{y}."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of ...."
+e = "a string with a right side."
+
+puts w + e
+
+# This tutorial language was taken directly from the Book "Learn Ruby the Hard Way"
