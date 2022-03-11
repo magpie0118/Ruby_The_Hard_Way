377e474f6790e099d17d7aa49140f6f51179624f
Maggie Fournier <mlfournier4@gmail.com>
2022-03-11 13:07:00 -0500

    Add files via upload
    Exercise 15 Reading and Printing from a File

---
+++ ex15.rb
@@ -0,0 +1,17 @@
+# Exercise 15 Reading Files
+
+# the file we are reading today is ex15_example.txt
+
+filename = ARGV.first
+
+txt = open(filename)
+
+puts "Here's your file #{filename}: "
+print txt.read
+
+print "Type the filename again:"
+file_again = $stdin.gets.chomp
+
+txt_again = open(file_again)
+
+print txt_again.read
