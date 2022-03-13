87f0ff806664a459ae0ea575aafb0f387f4ffcac
Maggie Fournier <mlfournier4@gmail.com>
2022-03-12 20:19:08 -0500

    Add files via upload
    Exercise 19 - Functions and Variables

--- ex19.rb
+++ ex19.rb
@@ -20,3 +20,25 @@ cheese_and_crackers(10 + 20, 5 + 6)
 
 puts "And we can combine the two, variables and math:"
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+# Additional Recommended Practice
+
+def ruby_lessons(lessons_complete, lessons_incomplete)
+    puts "You have completed #{lessons_complete} Ruby lessons"
+    puts "You have #{lessons_incomplete} Ruby lessons that you have not completed"
+    puts "You are making great progress!"
+    puts "Keep up the good work!"
+end
+
+ruby_lessons(19, 33)
+amount_of_lessons = 52
+percentage = "%"
+
+puts "How many Ruby lessons are required in total?"
+puts amount_of_lessons
+
+puts "What is the percentage of lessons completed?"
+print 19.0 / amount_of_lessons * 100
+puts percentage
+
+puts "Great job! Keep going"
